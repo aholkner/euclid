7fbf5e6ab70742dc73b8f00c9de4545fcaf3769a
Alex <alex@studiodrydock.com>
2019-08-11 17:35:01 +1000

    Import trunk from Google Code archive, convert README and replace LICENSE.

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+'''
+'''
+
+from distutils.core import setup
+
+setup(name='euclid',
+      version='0.01',
+      description='2D and 3D vector, matrix, quaternion and geometry module',
+      author='Alex Holkner',
+      author_email='alex@partiallydisassembled.net',
+      url='https://github.com/aholkner/euclid',
+      py_modules=['euclid'],
+      )
+
